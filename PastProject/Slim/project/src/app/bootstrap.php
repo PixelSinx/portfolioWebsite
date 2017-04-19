75f81d8b2f3028181dcdd471f37e97b32c09c3eb
Brody-PC\Brody <thextremewolfx@gmail.com>
2017-04-18 21:54:47 -0500

    Testing new code

--- PastProject/Slim/project/src/app/bootstrap.php
+++ PastProject/Slim/project/src/app/bootstrap.php
@@ -2,12 +2,6 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = new \Slim\App([
-    'settings' => [
-        'displayErrorDetails' => true,
-    ]
-]);
-
 $container = $app->getContainer();
 
 $container['view'] = function ($container) {
