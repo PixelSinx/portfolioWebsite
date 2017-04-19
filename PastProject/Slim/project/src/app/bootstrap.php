1d316c44ca382298127220b93faa554bf6d5ece1
Brody-PC\Brody <thextremewolfx@gmail.com>
2017-04-19 15:45:57 -0500

    Display error details turned on

--- PastProject/Slim/project/src/app/bootstrap.php
+++ PastProject/Slim/project/src/app/bootstrap.php
@@ -2,7 +2,11 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = new \Slim\App();
+$app = new \Slim\App([
+    'settings' => [
+        'displayErrorDetails' => true,
+    ]
+]);
 
 $container = $app->getContainer();
 
